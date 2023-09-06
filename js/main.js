b00d8e57b6edd2df0bf4ae38e47d15df4370da81
CarloCassini <c.cassini87@gmail.com>
2023-09-06 15:39:23 +0200

    done milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log("okokok");
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      emails: [],
+      // inserisci le variabili della app
+      // slides: []
+    };
+  },
+
+  methods: {
+    // inserisci i metodi
+    createMails(total) {
+      for (let i = 1; i <= total; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.emails.push(response.data.response);
+            console.table(this.emails);
+          });
+      }
+    },
+  },
+
+  created() {
+    this.createMails(10);
+    // inserisci app da lanciare all'avvio
+  },
+}).mount("#app");
